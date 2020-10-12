e8f3e2202aba139799c412de931601eb22bbef32
e-comns <e-comns@hotmail.com>
2020-10-12 12:52:47 -0400

    Python exercise

---
+++ main.py
@@ -0,0 +1,22 @@
+#fundamental data types
+int
+float
+bool
+str
+list
+tuple
+set
+dict
+
+#Classes -> custom types
+
+#Specialized data types
+
+None - data type for no value
+
+#Functions and some build it functions
+
+type - gives you info about data type
+round - math build in function gives us round numbers from float etc.
+
+ 
